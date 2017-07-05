47cbe455b6fbb19baece872bf87be00e1bc18e37
Patrick O'Brien <obrienp@outlook.com>
2017-07-05 08:52:44 -0500

    added car pojo, one endpoint

---
+++ src/main/java/com/cleo/rest/pojo/Car.java
@@ -0,0 +1,16 @@
+package com.cleo.rest.pojo;
+
+import java.util.UUID;
+
+public class Car {
+
+  private UUID id;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+}
--- src/main/java/com/cleo/rest/services/impl/CarsWebService.java
+++ src/main/java/com/cleo/rest/services/impl/CarsWebService.java
@@ -1,7 +1,18 @@
 package com.cleo.rest.services.impl;
 
+import com.cleo.rest.pojo.Car;
 import com.cleo.rest.services.ICarsWebService;
+import com.google.common.collect.Lists;
+import java.util.List;
+import javax.ws.rs.core.Response;
 
 public class CarsWebService implements ICarsWebService {
+
+  // Don't worry about persistin anything, just do CRUD operations on this list
+  private final static List<Car> cars = Lists.newArrayList();
   
+  @Override
+  public Response getAll() {
+    return Response.ok(new Car()).build();
+  }
 }
